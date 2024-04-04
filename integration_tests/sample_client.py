617632c2337a9bb3c5f43488adb2dbcf7f2ab02d
Prashant Gupta <prashantgupta@us.ibm.com>
2024-04-04 13:58:05 -0700

    🎨 add loop to client
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- integration_tests/sample_client.py
+++ integration_tests/sample_client.py
@@ -1,6 +1,5 @@
 import time
 import json
-from durations import Duration
 import grpc
 from google.protobuf import json_format
 from text_generation_tests.pb import generation_pb2_grpc as gpb2, generation_pb2 as pb2
@@ -26,47 +25,58 @@ def get_streaming_response_tgis(response):
 
 channel = grpc.insecure_channel("localhost:8033")
 stub = gpb2.GenerationServiceStub(channel)
-sample_request = {
-    "model_id": "dummy-model-name",
-    "request": {"text": "Hello! My favorite Bible verse is "},
-    "params": {
-        "method": "SAMPLE",
-        "sampling": {},
-        "decoding": {"repetition_penalty": 2},
-        "stopping": {
-            "min_new_tokens": 16,
-            "max_new_tokens": 20,
-            "stop_sequences": ["Peter ", "Timothy ", "joseph", "Corinthians"],
+max_new_tokens = 100
+
+template = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{}\n\n### Response:"
+num_req = 0
+while True:
+    prompt_input = input(f"\n{num_req}) Enter a prompt:\n")
+
+    print("\nOutput:")
+    prompt = template.format(prompt_input)
+    sample_request = {
+        "model_id": "dummy-model-name",
+        "request": {"text": prompt},
+        "params": {
+            "method": "GREEDY",
+            "stopping": {
+                "max_new_tokens": max_new_tokens,
+            },
         },
-    },
-}
-message = json_format.ParseDict(sample_request, pb2.SingleGenerationRequest())
-output = []
-t0 = time.time_ns()
-response = stub.GenerateStream(message)
-response_generator = get_streaming_response_tgis(response)
-
-stop = False
-while not stop:
-    r, n_tokens, t, ok, err = next(response_generator)
-
-    if not ok:
-        stop = True
-        # check if we have reached end of stream
-        if type(err) is StopIteration:
-            continue
-
-    record = {
-        "response": r,
-        "ok": ok,
-        "error": str(err),
-        "timestamp": t,
-        "duration_ms": (t - t0) / 1000.0 / 1000.0,
-        "n_tokens": n_tokens,
     }
+    message = json_format.ParseDict(sample_request, pb2.SingleGenerationRequest())
+    output = []
+    total_time = 0
+    response = stub.GenerateStream(message)
+    response_generator = get_streaming_response_tgis(response)
+    t0 = time.time_ns()
+    response = ""
+    stop = False
+    while not stop:
+        r, n_tokens, t, ok, err = next(response_generator)
 
-    output.append(record)
-    t0 = t
+        if not ok:
+            stop = True
+            # check if we have reached end of stream
+            if type(err) is StopIteration:
+                continue
+        duration = (t - t0) / 1000.0 / 1000.0
+        record = {
+            "response": r,
+            "ok": ok,
+            "error": str(err),
+            "timestamp": t,
+            "duration_ms": duration,
+            "n_tokens": n_tokens,
+        }
+        total_time += duration
+        response += r["text"]
+        output.append(record)
+        t0 = t
 
-with open("results.json", "w") as f:
-    json.dump(output, f)
+    # print(json.dumps(output, indent=4))
+    print(response)
+    print()
+    print(f"Total_time : {total_time}ms")
+    print(f"Time_per_token : {total_time/max_new_tokens}ms")
+    num_req += 1
